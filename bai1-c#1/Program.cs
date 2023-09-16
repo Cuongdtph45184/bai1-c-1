bc290ed703191c3a053b877c0cc5064268568550
Admin <Admin@CUONG123>
2023-09-16 23:11:24 +0700

    Add project files.

---
+++ bai1-c#1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1_c_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            int str;
+            do
+            {
+                Console.WriteLine("Sinh viên tham gia HappyBee");
+                Console.WriteLine("1.Nhập danh sách đối tượng");
+                Console.WriteLine("2.Xuất danh sách đối tượng");
+                Console.WriteLine("3.Xuất danh sách sinh viên được tặng vé HappyBee");
+                Console.WriteLine("4.Xóa sinh viên không được tham gia HappyBee nếu không được tặng");
+                Console.WriteLine("5.Kế Thừa");
+                Console.WriteLine("6.Thoát");
+                Console.Write("Vui lòng chọn chức năng :");
+                str = Convert.ToInt32(Console.ReadLine());
+                if(str == 6)
+                {
+                    break;
+                }
+                switch (str)
+                {
+                    case 1:
+                        Service.Nhap();
+                        break;
+                    case 2:
+                        Service.XuatDS();
+                        break;
+                    case 3:
+                        Service.DSSVHappyBee();
+                        break;
+                    case 4:
+                        Service.Xoa();
+                        break;
+                    case 5:
+                        kethua();
+                        break;
+                    default:
+                        break;
+
+                }
+
+            } while (str != 0);
+        }
+        static void kethua()
+        {
+            SinhVienUD svud = new SinhVienUD("ph45111", "Toan",2004,9,2);
+            svud.show();
+        }
+    }
+}
---
+++ bai1-c#1/SinhVienUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1_c_1
+{
+    internal class SinhVienUD : SinhVien
+    {
+        public int HocKy;
+        public SinhVienUD(string maSV, string ten, int namSinh, double diem, int kihoc) : base(maSV, ten, namSinh, diem)
+        {
+            this.HocKy = kihoc;
+        }
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Hoc ky: " + this.HocKy);
+        }
+    }
+}
+
